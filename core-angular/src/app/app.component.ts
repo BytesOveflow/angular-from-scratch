64e7df932cabd1bfbb6db18d01931c642aa4a551
prakashkmr746@gmail.com <prakashkmr000@PROPHEC.localdomain>
2022-01-01 19:25:54 +0530

    angular from scratch understood

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector :"main-app",
+  //  template : `h1`,
+    templateUrl: "./app.component.html",
+  //  styles: [`
+    //h1{
+      //  background-color : #FFF,
+    //}`   
+    
+    //],
+    styleUrls :["./app.component.css"]
+
+})
+export class AppComponent {}
---
+++ core-angular/src/app/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    styleUrls :["./card.component.css"],
+    selector: "card-app",
+    templateUrl: "./card.component.html"
+})
+export class CardComponent{
+
+}
